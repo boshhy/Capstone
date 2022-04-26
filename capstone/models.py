1baabc4958b54b8ba07b689643fa8b91d311f92b
boshhy <boshhy101@yahoo.com>
2022-04-26 15:19:48 -0700

    Added some models

--- capstone/models.py
+++ capstone/models.py
@@ -4,3 +4,23 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=64, blank=False)
+    description = models.TextField(blank=False)
+
+    def __str__(self):
+        return f"{self.title}"
+
+
+class Score(models.Model):
+    username = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="score")
+    game = models.ForeignKey(
+        Game, on_delete=models.CASCADE, related_name="score")
+    points = models.DecimalField(
+        max_digits=16, decimal_places=0, default=0)
+
+    def __str__(self):
+        return f"{self.points},  By: {self.username}, game: {self.game}"
