b10797ab08f3aec9f9c5c92378a716badceea382
boshhy <boshhy101@yahoo.com>
2022-05-03 23:49:59 -0700

    Added audio files for flappy bird

--- capstone/static/capstone/capstone.js
+++ capstone/static/capstone/capstone.js
@@ -1,4 +1,3 @@
-
 document.addEventListener("DOMContentLoaded", function () {
     title = document.getElementById('title');
 
@@ -13,6 +12,8 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 })
 
+running = false;
+
 function start_flappy() {
     canvas = document.getElementById('canvas')
     canvas.width = "0";
@@ -21,18 +22,20 @@ function start_flappy() {
     img.src = 'static/capstone/images/start_flappy.png';
     img.height = "512";
     img.width = "288";
+
     document.addEventListener("keydown", (event) => {
-        if (event.key === 's') {
-            img.style.display = 'none';
-            flappy();
-            return false;
+        if (!event.repeat) {
+            if (event.key === 's' && !running) {
+                img.style.display = 'none';
+                flappy();
+            }
         }
     })
 }
 
 
 function flappy() {
-
+    running = true;
     console.log("you are now running flappy bird");
     var canvas = document.getElementById('canvas');
     canvas.width = "288";
@@ -44,12 +47,18 @@ function flappy() {
     var ground = new Image();
     var pipeup = new Image();
     var pipedown = new Image();
+    //var fly = new Audio();
+    var point = new Audio();
+    var hit = new Audio();
 
     bird.src = '/static/capstone/images/bird.png';
     background.src = 'static/capstone/images/background.png';
     ground.src = 'static/capstone/images/ground.png';
     pipeup.src = 'static/capstone/images/pipeup.png';
     pipedown.src = 'static/capstone/images/pipedown.png';
+    hit.src = 'static/capstone/sounds/hit.wav';
+    point.src = 'static/capstone/sounds/point.mp3';
+    //fly.src = 'static/capstone/sounds/fly.mp3';
 
     if (pipeup.height == 0) {
         pipeup.height = 242;
@@ -63,14 +72,18 @@ function flappy() {
     var score = 0;
 
     document.addEventListener("keydown", (event) => {
-        if (event.key === 'k') {
-            moveUp();
+        if (!event.repeat) {
+            if (event.key === 'k' && running) {
+                moveUp();
+            }
         }
     })
 
-
     function moveUp() {
-        bY -= 50
+        bY -= 50;
+        //fly.pause();
+        //fly.currentTime = 0;
+        //fly.play();
     }
 
     var pipe = [];
@@ -78,10 +91,7 @@ function flappy() {
     pipe[0] = {
         x: canvas.width,
         y: 0
-
     }
-
-
 
 
     function draw() {
@@ -108,15 +118,33 @@ function flappy() {
                     ||
                     bY + bird.height >= pipe[i].y + constant)
                 || (bY + bird.height >= canvas.height - ground.height)) {
-                go_fetch(score, "flappy_bird")
-                flappy();
-                return false;
+
+                go_fetch(score, "flappy_bird");
+                running = false;
+                //flappy();
+
+
+                ctx.drawImage(pipeup, pipe[i].x, pipe[i].y);
+                ctx.drawImage(pipedown, pipe[i].x, pipe[i].y + constant);
+                ctx.drawImage(ground, 0, canvas.height - ground.height);
+                ctx.drawImage(bird, bX, bY);
+                ctx.fillstyle = "#000";
+                ctx.font = "26px Verdana";
+                ctx.fillText("Score: " + score, 10, canvas.height - 20);
+                hit.volume = 0.4;
+                hit.play();
+                //fly.pause();
+                //fly.currentTime = 0;
+                //flappy();
+                return;
             }
 
-            if (pipe[i].x == 6) {
+            if (pipe[i].x == -32) {
+                point.play();
                 score = score + 1;
             }
         }
+
         ctx.drawImage(ground, 0, canvas.height - ground.height);
         ctx.drawImage(bird, bX, bY);
 
@@ -124,29 +152,9 @@ function flappy() {
         ctx.fillstyle = "#000";
         ctx.font = "26px Verdana";
         ctx.fillText("Score: " + score, 10, canvas.height - 20);
-
         requestAnimationFrame(draw);
     }
-
-
     draw();
-
-
-
-
-
-    // END
-
-
-    // fetch('/score', {
-    //     method: 'POST',
-    //     body: JSON.stringify({
-    //         'points': randomx,
-    //         'game': 'flappy_bird',
-    //     })
-    // })
-    //     .then(result => result.json())
-    //     .then(console.log('we have saved a score for flappy bird'))
 }
 
 function go_fetch(points, game) {
@@ -175,6 +183,19 @@ function go_fetch(points, game) {
         })
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 function snake() {
     console.log("you are now running snake");
     fetch('/score', {
@@ -187,3 +208,5 @@ function snake() {
         .then(result => result.json())
         .then(console.log('we have saved a score for snake'))
 }
+
+
