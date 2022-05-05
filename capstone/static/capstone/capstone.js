c7ac59e86f79ed75a0d09a210fc32acd0920e6ab
boshhy <boshhy101@yahoo.com>
2022-05-04 18:31:09 -0700

    Added snake images and game logic

--- capstone/static/capstone/capstone.js
+++ capstone/static/capstone/capstone.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
         start_flappy();
     }
     else if (title.innerHTML == "Snake") {
-        snake();
+        start_snake();
     }
     else {
         console.log("You are not on a game page")
@@ -26,8 +26,30 @@ function start_flappy() {
     document.addEventListener("keydown", (event) => {
         if (!event.repeat) {
             if (event.key === 's' && !running) {
+                canvas.width = "288";
+                canvas.height = "512";
                 img.style.display = 'none';
                 flappy();
+            }
+        }
+    })
+}
+
+function start_snake() {
+    canvas = document.getElementById('canvas')
+    canvas.width = "0";
+    canvas.height = "0";
+    img = document.getElementById("start_image");
+    img.src = 'static/capstone/images/start_snake.png';
+    img.height = "608";
+    img.width = "608";
+    document.addEventListener("keydown", (event) => {
+        if (!event.repeat) {
+            if (event.key === 's' && !running) {
+                canvas.width = "608";
+                canvas.height = "608";
+                img.style.display = 'none';
+                snake();
             }
         }
     })
@@ -38,8 +60,6 @@ function flappy() {
     running = true;
     console.log("you are now running flappy bird");
     var canvas = document.getElementById('canvas');
-    canvas.width = "288";
-    canvas.height = "512";
     var ctx = canvas.getContext('2d');
 
     var bird = new Image();
@@ -197,11 +217,122 @@ function go_fetch(points, game) {
 
 
 function snake() {
+    running = true;
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+
+    const box = 32;
+
+    const floor = new Image();
+    floor.src = "static/capstone/images/floor.png";
+
+    const foodImg = new Image();
+    foodImg.src = "static/capstone/images/food.png";
+
+    let snake = [];
+
+    snake[0] = {
+        x: 9 * box,
+        y: 10 * box
+    };
+
+    let food = {
+        x: Math.floor(Math.random() * 17 + 1) * box,
+        y: Math.floor(Math.random() * 15 + 3) * box
+    }
+
+    let score = 0;
+
+    let d;
+
+    document.addEventListener("keydown", direction);
+
+    function direction(event) {
+        event.preventDefault();
+        if (event.keyCode == 37 && d != "RIGHT") {
+            d = "LEFT";
+        } else if (event.keyCode == 38 && d != "DOWN") {
+            d = "UP";
+        } else if (event.keyCode == 39 && d != "LEFT") {
+            d = "RIGHT";
+        } else if (event.keyCode == 40 && d != "UP") {
+            d = "DOWN";
+        }
+    }
+
+    function collision(head, array) {
+        for (let i = 0; i < array.length; i++) {
+            if (head.x == array[i].x && head.y == array[i].y) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function draw() {
+        ctx.drawImage(floor, 0, 0);
+
+        for (let i = 0; i < snake.length; i++) {
+            ctx.fillStyle = (i == 0) ? "green" : "white";
+            ctx.fillRect(snake[i].x, snake[i].y, box, box);
+
+            ctx.strokeStyle = "red";
+            ctx.strokeRect(snake[i].x, snake[i].y, box, box);
+        }
+        ctx.drawImage(foodImg, food.x, food.y);
+
+
+        let snakeX = snake[0].x;
+        let snakeY = snake[0].y;
+
+
+
+        if (d == "LEFT") snakeX -= box;
+        if (d == "UP") snakeY -= box;
+        if (d == "RIGHT") snakeX += box;
+        if (d == "DOWN") snakeY += box;
+
+        if (snakeX == food.x && snakeY == food.y) {
+            score++;
+            food = {
+                x: Math.floor(Math.random() * 17 + 1) * box,
+                y: Math.floor(Math.random() * 15 + 3) * box
+            }
+        } else {
+            snake.pop();
+        }
+
+        let newHead = {
+            x: snakeX,
+            y: snakeY
+        }
+
+        if (snakeX < box || snakeX > 17 * box
+            || snakeY < 3 * box || snakeY > 17 * box
+            || collision(newHead, snake)) {
+            clearInterval(game);
+            running = false;
+        }
+
+
+        snake.unshift(newHead);
+
+        ctx.fillStyle = "white";
+        ctx.font = "45px Changa one";
+        ctx.fillText(score, 2 * box, 1.6 * box);
+
+    }
+
+    let game = setInterval(draw, 128);
+
+
+
+
     console.log("you are now running snake");
     fetch('/score', {
         method: 'POST',
         body: JSON.stringify({
-            'points': randomx,
+            'points': 2,
             'game': 'snake',
         })
     })
